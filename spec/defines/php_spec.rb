2c147c265d192bd6777809d86b3b03f2f2d41ff8
Fireblade <m.greenhalgh@elsevier.com>
2016-04-28 11:41:27 +0100

    Added php rspec

---
+++ spec/defines/php_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+describe 'newrelic::php' do
+
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts
+      end
+
+      let(:title) {'os'}
+      let(:params) {{
+          :key    => '8fb13cf1873e95f60045afe67fc3a6aba9e563da',
+          :ensure => 'present',
+        }}
+
+      it { is_expected.to have_resource_count(4)}
+
+      it 'should generate valid content for newrelic-install' do
+        content = catalogue.resource('file', '/usr/bin/newrelic-install').send(:parameters)[:content]
+        expect(content).not_to be_empty
+      end
+
+      it { is_expected.to contain_exec('/usr/bin/newrelic-install install').with(
+        :creates => '/usr/bin/newrelic-install',
+        :require => 'Package[newrelic-php5]',
+      )}
+
+      case facts[:osfamily]
+      when 'RedHat'
+
+        it { is_expected.to contain_package('newrelic-php5').with(
+          :ensure  => 'present',
+          :require => 'Yumrepo[newrelic]',
+        )}
+
+        it { is_expected.to contain_file('/usr/bin/newrelic-install').with(
+          :ensure  => 'present',
+          :mode    => '0755',
+          :owner   => 'root',
+          :group   => 'root',
+        )}
+      end
+    end
+  end
+end
